c5a786d86fb7b172a81d20c1ff41409b794457eb
advico <adriawndev@gmail.com>
2024-04-17 23:16:21 -0300

    desafio concluido!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,32 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class iPhone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public string TouchID { get; set; }
+        public string FaceID { get; set; }
+
+        public iPhone(string numero, string modelo, string imei, int memoria, string touchID, string faceID) 
+            : base(numero, modelo, imei, memoria)
+        {
+            TouchID = touchID;
+            FaceID = faceID;
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no iPhone");
+        }
+
+        public void DesbloquearComTouchID()
+        {
+            Console.WriteLine("Desbloqueando com Touch ID...");
+        }
+
+        public void DesbloquearComFaceID()
+        {
+            Console.WriteLine("Desbloqueando com Face ID...");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
-﻿using DesafioPOO.Models;
+﻿using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando uma instância de Nokia
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 64);
+
+            // Testando métodos da classe Nokia
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            // Criando uma instância de iPhone
+            iPhone iphone = new iPhone("987654321", "iPhone 12", "987654321098765", 128, "Touch ID", "Face ID");
+
+            // Testando métodos da classe iPhone
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+            iphone.DesbloquearComTouchID();
+            iphone.DesbloquearComFaceID();
+        }
+    }
+}
